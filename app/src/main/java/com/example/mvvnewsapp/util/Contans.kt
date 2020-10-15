bde995208db8986b5e1379e9126849a8352a00a6
Athoriq1224 <athoriq2004@gmail.com>
2020-10-15 09:40:52 +0700

    MVVMNewsAPP

---
+++ app/src/main/java/com/example/mvvnewsapp/util/Contans.kt
@@ -0,0 +1,9 @@
+package com.example.mvvnewsapp.util
+
+class Contans{
+    companion object{
+        const val API_KEY  = "1f4c1695288b460fa69c95f86b575e51"
+        const val BASE_URL = "https://newsapi.org/"
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
